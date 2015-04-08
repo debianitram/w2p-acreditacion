a8d3ce787ffc288b1a024cb962b889bff2d8dc8b
adrian <adrianmarello_22@hotmail.com>
2015-04-08 12:39:51 -0300

    diseño de vista de curso

--- controllers/persona.py
+++ controllers/persona.py
@@ -28,3 +28,36 @@ def index():
                         )
 
     return dict(grid=grid)
+
+def inscripto():
+	grid2 = SQLFORM.grid(Inscripto,
+                        csv=False,
+                        maxtextlength=200,
+                        ondelete=hide_record,
+                        user_signature=True,
+                        showbuttontext=False,
+                        orderby=~Inscripto.created_on,
+                        )
+	return dict(grid2=grid2)
+
+def pagos():
+	grid3 = SQLFORM.grid(Pagos,
+                        csv=False,
+                        maxtextlength=200,
+                        ondelete=hide_record,
+                        user_signature=True,
+                        showbuttontext=False,
+                        orderby=~Pagos.created_on,
+                        )
+	return dict(grid3=grid3)
+
+def asistencia():
+	grid4 = SQLFORM.grid(Asistencia,
+                        csv=False,
+                        maxtextlength=200,
+                        ondelete=hide_record,
+                        user_signature=True,
+                        showbuttontext=False,
+                        orderby=~Asistencia.created_on,
+                        )
+	return dict(grid4=grid4)
