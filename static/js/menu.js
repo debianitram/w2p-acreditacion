6b4cfbbd5e7e20c889cac9853050da6ac12d0c89
adrian <adrianmarello_22@hotmail.com>
2015-04-15 23:27:43 -0300

    fixed menu

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,11 +1,12 @@
 /*   MENU  DROPDOWN AOM */
 $(".dropdown").hover(
     function(){
-        $('.dropdown-menu', this).stop(true, true).slideDown("medium");
+        $('.dropdown-menu', this).stop(true, false).slideDown("medium");
         $(this).toggleClass('open');        
     },
     function(){
-        $('.dropdown-menu', this).stop(true, true).slideUp("medium");
+        $('.dropdown-menu', this).stop(true, false).slideUp("medium");
         $(this).toggleClass('open');       
     }
 );
+
