4ef233b90317c60f81e7d6bd076b73823bc1e31e
Martín Miranda <debianitram@gmail.com>
2015-04-17 11:16:24 -0300

    grid.view_form.custom to object

--- languages/es.py
+++ languages/es.py
@@ -215,6 +215,7 @@
 'Forgot username?': '¿Olvidó el nombre de usuario?',
 'Forms and Validators': 'Formularios y validadores',
 'Free Applications': 'Aplicaciones Libres',
+'Fsearch': 'Fsearch',
 'Functions with no doctests will result in [passed] tests.': 'Funciones sin doctests equivalen a pruebas [aceptadas].',
 'Group %(group_id)s created': 'Grupo %(group_id)s creado',
 'Group ID': 'ID de Grupo',
