fc521f0776440cab7a9838adb20c7f3058e4f9db
Martín Miranda <debianitram@gmail.com>
2015-05-14 10:53:40 -0300

    reports presence of curso

--- controllers/inscripto.py
+++ controllers/inscripto.py
@@ -1,13 +1,6 @@
 #!-*- encoding: utf-8 -*-
 
-import StringIO
-from os import path
-from decimal import Decimal
-from xhtml2pdf.pisa import CreatePDF
-
 from gluon.serializers import json
-
-from curso_aux import date_reportcert, time_reportcert
 
 
 @auth.requires_login()
@@ -190,90 +183,3 @@ def actions_process():
 
         else:
             return hide_modal + 'alert("No tiene permisos suficientes");'
-
-
-
-
-def certificados():
-    response.view = 'reports/certificado.html'
-
-    curso = Curso(request.args(0, cast=int))
-    fechas = curso.cfecha.select(CFecha.fecha,
-                                 CFecha.hora_inicio,
-                                 CFecha.hora_fin)
-
-    text_date = date_reportcert(fechas)
-    text_hours_count = time_reportcert(fechas)
-    
-    if 'all' in request.args:
-        inscriptos = curso.inscripto.select(
-            Inscripto.persona,
-            Inscripto.docente,
-            Inscripto.acreditado
-        ).find(lambda r: r['acreditado'])
-    else:
-        inscriptos = curso.inscripto(Inscripto.id == request.args(-1)).select()
-        if not inscriptos.first()['acreditado']:
-            response.flash = 'Necesita acreditarlo para poder imprimir el certificado'
-            redirect(URL(c='curso',
-                         f='index',
-                         extension='html',
-                         args=('view', Curso._tablename, curso.id),
-                         user_signature=True))
-
-
-    context = dict(curso=curso,
-                   inscriptos=inscriptos,
-                   text_date=text_date,
-                   count_hours=text_hours_count)
-
-    if request.extension == 'pdf':
-        path_css = path.join(request.env.web2py_path,
-                                'applications/init/static/css/reporte_cert.css')
-        
-        with open(path_css, 'r') as rcss:
-            css = rcss.read()
-
-        html = response.render(response.view, context)
-        doc = StringIO.StringIO()
-        pdf = CreatePDF(html,
-                        dest=doc,
-                        default_css=css,
-                        encoding='utf-8')
-        return doc.getvalue()
-    return context
-
-
-def asistencias():
-    response.view = 'reports/asistencias.html'
-
-    curso = Curso(request.args(0, cast=int))
-    fechas = curso.cfecha.select(CFecha.fecha,
-                                 CFecha.hora_inicio,
-                                 CFecha.hora_fin)
-    row_inscriptos = curso.inscripto.select(Inscripto.persona, Inscripto.docente)
-    docentes = row_inscriptos.find(lambda r: r['docente'])
-    inscriptos = row_inscriptos.find(lambda r: not r['docente'])
-
-    context = dict(curso=curso,
-                   fechas=fechas,
-                   docentes=docentes,
-                   inscriptos=inscriptos)
-
-    if request.extension == 'pdf':
-        path_static = path.join(request.env.web2py_path,
-                                'applications/init/static')
-        
-        with open(path.join(path_static, 'css/reporte_asist.css'), 'r') as rcss:
-            css = rcss.read()
-
-        html = response.render(response.view, context)
-        doc = StringIO.StringIO()
-        pdf = CreatePDF(html,
-                        dest=doc,
-                        path=path.join(path_static, 'images'),
-                        default_css=css,
-                        encoding='utf-8')
-        return doc.getvalue()
-    return context
-
---
+++ controllers/reports.py
@@ -0,0 +1,91 @@
+#!-*- encoding: utf-8 -*-
+
+import StringIO
+from os import path
+from xhtml2pdf.pisa import CreatePDF
+
+
+from curso_aux import date_reportcert, time_reportcert
+
+
+def certificados():
+    response.view = 'reports/certificado.html'
+
+    curso = Curso(request.args(0, cast=int))
+    fechas = curso.cfecha.select(CFecha.fecha,
+                                 CFecha.hora_inicio,
+                                 CFecha.hora_fin)
+
+    text_date = date_reportcert(fechas)
+    text_hours_count = time_reportcert(fechas)
+    
+    if 'all' in request.args:
+        inscriptos = curso.inscripto.select(
+            Inscripto.persona,
+            Inscripto.docente,
+            Inscripto.acreditado
+        ).find(lambda r: r['acreditado'])
+    else:
+        inscriptos = curso.inscripto(Inscripto.id == request.args(-1)).select()
+        if not inscriptos.first()['acreditado']:
+            response.flash = 'Necesita acreditarlo para poder imprimir el certificado'
+            redirect(URL(c='curso',
+                         f='index',
+                         extension='html',
+                         args=('view', Curso._tablename, curso.id),
+                         user_signature=True))
+
+
+    context = dict(curso=curso,
+                   inscriptos=inscriptos,
+                   text_date=text_date,
+                   count_hours=text_hours_count)
+
+    if request.extension == 'pdf':
+        path_css = path.join(request.env.web2py_path,
+                                'applications/init/static/css/reporte_cert.css')
+        
+        with open(path_css, 'r') as rcss:
+            css = rcss.read()
+
+        html = response.render(response.view, context)
+        doc = StringIO.StringIO()
+        pdf = CreatePDF(html,
+                        dest=doc,
+                        default_css=css,
+                        encoding='utf-8')
+        return doc.getvalue()
+
+
+def asistencias():
+    response.view = 'reports/asistencias.html'
+
+    curso = Curso(request.args(0, cast=int))
+    fechas = curso.cfecha.select(CFecha.fecha,
+                                 CFecha.hora_inicio,
+                                 CFecha.hora_fin)
+    row_inscriptos = curso.inscripto.select(Inscripto.persona, Inscripto.docente)
+    docentes = row_inscriptos.find(lambda r: r['docente'])
+    inscriptos = row_inscriptos.find(lambda r: not r['docente'])
+
+    context = dict(curso=curso,
+                   fechas=fechas,
+                   docentes=docentes,
+                   inscriptos=inscriptos)
+
+    if request.extension == 'pdf':
+        path_static = path.join(request.env.web2py_path,
+                                'applications/init/static')
+        
+        with open(path.join(path_static, 'css/reporte_asist.css'), 'r') as rcss:
+            css = rcss.read()
+
+        html = response.render(response.view, context)
+        doc = StringIO.StringIO()
+        pdf = CreatePDF(html,
+                        dest=doc,
+                        path=path.join(path_static, 'images'),
+                        default_css=css,
+                        encoding='utf-8')
+        return doc.getvalue()
+
